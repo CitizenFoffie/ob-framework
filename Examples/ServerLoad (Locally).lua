da75cc4ea10de2a57105bc8a3e81b854cc3ba6c0
p0s0 <73150246+p0s0@users.noreply.github.com>
2021-09-14 15:24:54 -0400

    Update ServerLoad (Locally).lua

--- Examples/ServerLoad (Locally).lua
+++ Examples/ServerLoad (Locally).lua
@@ -1,5 +1,5 @@
 -- assuming this is inside of ServerScriptService
 
-local obFramework = require(script.Parent:WaitForChild("OBFramework"))
+local obFramework = require(script.Parent:WaitForChild("MainModule")) -- change MainModule to what you named the script, or if you didn't rename, keep it as it is.
 
 obFramework:Load("2015E")
